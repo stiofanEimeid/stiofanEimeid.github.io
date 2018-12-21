2fd0c62a871829a4991e7427bb7566522b44ff0a
Stephen Byrne <44100760+stiofanEimeid@users.noreply.github.com>
2018-12-21 23:32:38 +0000

    buttons selected for event listener tests

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-// alert("connected");
+var btn1 = document.querySelector("button")[1];
+var btn2 = document.querySelector("button")[2];
+var btn3 = document.querySelector("button")[3];
+var btn4 = document.querySelector("button")[4];
+
+btn1.addEventListener("click", alert("hi"));
+btn2.addEventListener("click", alert("hola"));
+btn3.addEventListener("click", alert("bonjour"));
+btn4.addEventListener("click", alert("dia duit"));
