c634ea7b61a4d10ccb35d3a5fcf9dadc4ad908a2
Stephen Byrne <44100760+stiofanEimeid@users.noreply.github.com>
2018-12-21 23:55:38 +0000

    js fixed
    Alert commands contained in anonymous functions and buttons identified correctly.

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-var btn1 = document.querySelector("button")[1];
-var btn2 = document.querySelector("button")[2];
-var btn3 = document.querySelector("button")[3];
-var btn4 = document.querySelector("button")[4];
+var btn1 = document.querySelector(".tablink:nth-of-type(1)");
+var btn2 = document.querySelector(".tablink:nth-of-type(2)");
+var btn3 = document.querySelector(".tablink:nth-of-type(3)");
+var btn4 = document.querySelector(".tablink:nth-of-type(4)");
 
-btn1.addEventListener("click", alert("hi"));
-btn2.addEventListener("click", alert("hola"));
-btn3.addEventListener("click", alert("bonjour"));
-btn4.addEventListener("click", alert("dia duit"));
+btn1.addEventListener("click", function(){ alert("hi")});
+btn2.addEventListener("click", function(){alert("hola")});
+btn3.addEventListener("click", function(){alert("bonjour")});
+btn4.addEventListener("click", function(){alert("dia duit")});
