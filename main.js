6324e6744b484ee9548a2050eeeb6a9547853813
Stephen Byrne <44100760+stiofanEimeid@users.noreply.github.com>
2018-12-24 21:15:29 +0000

    Surplus js removed

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
-function openPage(pageName,elmnt,color) {
-  var i, 
-  tabcontent, 
-  tablinks;
-
-  tabcontent = document.getElementsByClassName("tabcontent");
+function openPage(pageName,elmnt) {
+  
+  tabcontent = document.querySelectorAll(".tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
-  tablinks = document.getElementsByClassName("tablink");
+  tablinks = document.querySelectorAll(".tablink");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].style.backgroundColor = "";
   }
   document.getElementById(pageName).style.display = "block";
-  elmnt.style.backgroundColor = color;
+  elmnt.style.backgroundColor = "white";
 }
 
 document.querySelector("#defaultOpen").click();
