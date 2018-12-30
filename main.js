9b734d90889b15c29a43c4a50a85ae1a8b76e191
Stephen Byrne <44100760+stiofanEimeid@users.noreply.github.com>
2018-12-30 21:39:36 +0000

    tab js deactivated

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-function openPage(pageName,elmnt) {
+// function openPage(pageName,elmnt) {
   
-  tabcontent = document.querySelectorAll(".tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  tablinks = document.querySelectorAll(".tablink");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].style.backgroundColor = "";
-  }
-  document.getElementById(pageName).style.display = "block";
-  elmnt.style.backgroundColor = "white";
-}
+//   tabcontent = document.querySelectorAll(".tabcontent");
+//   for (i = 0; i < tabcontent.length; i++) {
+//     tabcontent[i].style.display = "none";
+//   }
+//   tablinks = document.querySelectorAll(".tablink");
+//   for (i = 0; i < tablinks.length; i++) {
+//     tablinks[i].style.backgroundColor = "";
+//   }
+//   document.getElementById(pageName).style.display = "block";
+//   elmnt.style.backgroundColor = "white";
+// }
 
-document.querySelector("#defaultOpen").click();
+// document.querySelector("#defaultOpen").click();
